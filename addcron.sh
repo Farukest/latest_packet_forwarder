526a3097bdb7500e862d4de4b5e4a694f97ebb19
farukest <abdullahfarukozden@gmail.com>
2023-12-19 01:15:07 +0300

    firstrun

--- addcron.sh
+++ addcron.sh
@@ -1,4 +1,4 @@
 (crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh
- */10 * * * *    /home/latest_packet_forwarder/autowireguard.sh
+ */1 * * * *  /bin/bash  /home/latest_packet_forwarder/autowireguard.sh
  
  "; ) | crontab -
