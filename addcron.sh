65407c118f9527555c9fa1ad783d5a0ef244b569
farukest <abdullahfarukozden@gmail.com>
2023-12-18 03:19:04 +0300

    autowreguard

--- addcron.sh
+++ addcron.sh
@@ -1,4 +1,4 @@
-crontab -r
-{ crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh
-*/10 * * * *    /bin/bash /home/latest_packet_forwarder/autowireguard.sh
-"; ) | crontab -
+(crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh
+ */10 * * * *    /home/latest_packet_forwarder/autowireguard.sh
+ 
+ "; ) | crontab -
