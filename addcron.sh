0986025ce561f4516022f5c11d1e515dc4148bcb
farukest <abdullahfarukozden@gmail.com>
2023-12-18 03:14:01 +0300

    autowreguard

--- addcron.sh
+++ addcron.sh
@@ -1,2 +1,4 @@
 crontab -r
-{ crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh"; } | crontab -
+{ crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh
+*/10 * * * *    /bin/bash /home/latest_packet_forwarder/autowireguard.sh
+"; ) | crontab -
---
+++ autowireguard.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+hasFault=0
+
+echo "Link detection..."
+/usr/bin/wg | grep -q "interface: wg0"
+if [ $? -ne 0 ]; then
+    hasFault=1
+    echo "Link down!"
+else
+    echo "Ok!"
+fi
+echo ""
+
+echo "Ping detection..."
+ping -c 1 10.66.66.1
+if [ $? -ne 0 ]; then
+    hasFault=1
+    echo "Ping failed!"
+else
+    echo "Ok!"
+fi
+echo ""
+
+echo "Status"
+if [ $hasFault -eq 0 ]; then
+    echo "Ok!"
+    exit
+fi
+
+echo "Error!"
+echo 'Restart...'
+
+sudo wg-quick down wg0
+sleep 5
+sudo wg-quick up wg0
+
+# systemctl restart wg-quick@wg0.service
+# systemctl --no-pager status wg-quick@wg0.service
