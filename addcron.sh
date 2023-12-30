45a385a1e04d8bae14f996e51b9681b775e9f74b
Farukest <abdullahfarukozden@gmail.com>
2023-12-30 21:08:28 +0300

    changes cron

--- addcron.sh
+++ addcron.sh
@@ -1,4 +1,3 @@
 (crontab -l; echo "  * *   *   *   *    /bin/bash /home/latest_packet_forwarder/runall.sh
- */1 * * * *  /bin/bash  /home/latest_packet_forwarder/autowireguard.sh
  
  "; ) | crontab -
